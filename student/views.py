ae52e9433a7a40df759906a0ac988bc6d3385b08
nuengnakhap <nueng1569@hotmail.com>
2020-01-30 09:39:16 +0700

    post student

--- student/views.py
+++ student/views.py
@@ -1,10 +1,16 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+
 from student.models import student
+
 # Create your views here.
 
 
 def index(request):
     if request.method == 'POST':
-        student.append(request.body)
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        student.append(body)
     return JsonResponse(student, safe=False)
